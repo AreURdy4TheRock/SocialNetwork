3398aa48e2cddb7a4306dca95de4aa440ecc723a
AreURdy4TheRock <dorkwingdock@gmail.com>
2023-06-18 23:52:31 +0300

    Добавьте файлы проекта.

---
+++ SocialNetwork/BLL/Services/UserService.cs
@@ -0,0 +1,66 @@
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.DAL.Entities;
+using SocialNetwork.DAL.Repositories;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.BLL.Services
+{
+    public class UserService
+    {
+        IUserRepository userRepository;
+        public UserService()
+        {
+            userRepository = new UserRepository();
+        }
+        public void Register(UserRegistrarionData userRegistrarionData)
+        {
+            if (String.IsNullOrEmpty(userRegistrarionData.FirstName))
+            {
+                throw new ArgumentException();
+            }
+            if (String.IsNullOrEmpty (userRegistrarionData.LastName))
+            {
+                throw new ArgumentException();
+            }
+            if (String.IsNullOrEmpty (userRegistrarionData.Password))
+            {
+                throw new ArgumentException();
+            }
+            if (String.IsNullOrEmpty (userRegistrarionData.Email))
+            {
+                throw new ArgumentException();
+            }
+
+            if (userRegistrarionData.Password.Length < 8)
+            {
+                throw new ArgumentException();
+            }
+            if (!new EmailAddressAttribute().IsValid(userRegistrarionData.Email))
+            {
+                throw new ArgumentException();
+            }
+            if(userRepository.FindByEmail(userRegistrarionData.Email) != null)
+            {
+                throw new ArgumentException();
+            }
+
+            var userEntity = new UserEntity()
+            {
+                firstname = userRegistrarionData.FirstName,
+                lastname = userRegistrarionData.LastName,
+                email = userRegistrarionData.Email,
+                password = userRegistrarionData.Password
+            };
+            if (this.userRepository.Create(userEntity) == 0) 
+            {
+                throw new Exception();
+            }
+
+        }
+    }
+}
---
+++ SocialNetwork/Program.cs
@@ -0,0 +1,52 @@
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork
+{
+    public class Program
+    {
+        public static UserService userService = new UserService();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в социальную сеть!");
+            while (true)
+            {
+                Console.Write("Для регистрации пользователя введите имя: ");
+                string firstName = Console.ReadLine();
+                Console.Write("фамилию: ");
+                string lastName = Console.ReadLine();
+                Console.Write("пароль: ");
+                string password = Console.ReadLine();
+                Console.Write("почтовый адрес: ");
+                string email = Console.ReadLine();
+                UserRegistrarionData userRegistrarionData = new UserRegistrarionData()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Password = password,
+                    Email = email
+                };
+
+                try
+                {
+                    userService.Register(userRegistrarionData);
+                    Console.WriteLine("Регистрация произошла успешно!");
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Введите корректное значение!");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Произошла ошибка при регистрации!");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
